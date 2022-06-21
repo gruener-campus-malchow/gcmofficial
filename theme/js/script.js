cb54af682fd289e3b8dd890cc94ed94e9b2a728c
TenkouNoKitsune <66792136+TenkouNoKitsune@users.noreply.github.com>
2022-06-21 12:06:26 +0200

    added search to mobile site via js

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,10 +1,22 @@
 // ensure that every element has been loaded
 window.onload = function () {
-    var toggle = document.getElementById("superfish-main-toggle");
-    var menu = document.getElementById("block-hauptnavigation");
+    // mobile browsers only
+    if (window.innerWidth / window.innerHeight <= 0.7) {
+        var toggle = document.getElementById("superfish-main-toggle");
+        var menu = document.getElementById("block-hauptnavigation");
 
-    toggle.addEventListener("click", () => {
-        // the class only exists in the css breaks/mobile.css
-        menu.classList.toggle("mobile-extentmenu");
-    });
+        toggle.addEventListener("click", () => {
+            // the class can be found in the css breaks/mobile.css
+            menu.classList.toggle("mobile-extentmenu");
+        });
+
+        var search = document.getElementById("block-gcmofficial-search");
+        var menulist = document.getElementById("superfish-main-accordion");
+
+        search.parentNode.removeChild(search);
+        listitem = document.createElement("li");
+        listitem.classList.add("sf-depth-1", "sf-no-children");
+        listitem.appendChild(search);
+        menulist.prepend(listitem);
+    }
 };
