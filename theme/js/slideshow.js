f7c68a63f9e3cc6b50fb9f4666daf2e868bce11b
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2022-06-29 19:14:23 +0200

    duck

--- theme/js/slideshow.js
+++ theme/js/slideshow.js
@@ -1,13 +1,30 @@
 document.addEventListener("DOMContentLoaded", main());
+document.getElementById('slideshow').onclick = toggle();
 
 function toggle() {
-  
+    window.clearTimeout(timeoutID);
 }
 
 async function autoswitch() {
-
+    slideIndex = 1
+    while (True) {
+        timeoutID = window.setTimeout(showslide(), 5000);
+        let slides = document.getElementsByClassName("field__item");
+        if (slideIndex == slides.length-1) {
+            slideIndex = 0
+        } else {
+            slideIndex += 1;
+        }
+        showslide(slideIndex)
+    }
+    timeoutID = window.setTimeout(showslide(slideIndex), 2000);
 };
-
+function showslide(slideIndex) {
+    let slides = document.getElementsByClassName("field__item");
+    let i = 0
+    for (i in slides.length-1){slides[i].style.visibility = "hidden";}
+    slides[slideIndex].style.display = "visible";
+}
 function main() {
-
+    autoswitch();
 };
