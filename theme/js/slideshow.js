00ae75f85897e3681e215d854efd17cae96f91c3
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2022-07-12 15:55:20 +0200

    duck

--- theme/js/slideshow.js
+++ theme/js/slideshow.js
@@ -1,30 +1,31 @@
 document.addEventListener("DOMContentLoaded", main());
 document.getElementById('slideshow').onclick = toggle();
-
-function toggle() {
-    window.clearTimeout(timeoutID);
-}
-
+// Switches SLides every 5 seconds
 async function autoswitch() {
     slideIndex = 1
-    while (True) {
-        timeoutID = window.setTimeout(showslide(), 5000);
-        let slides = document.getElementsByClassName("field__item");
+    return setInterval(function () {
+        let slides = document.getElementById("titelbild").document.getElementsByClassName("field__item");
         if (slideIndex == slides.length-1) {
             slideIndex = 0
         } else {
             slideIndex += 1;
         }
         showslide(slideIndex)
-    }
-    timeoutID = window.setTimeout(showslide(slideIndex), 2000);
-};
+    }, 5000);
+}
+// imideatly switches slides
+function toggle() {
+    window.clearInterval(timerID);
+}
+// shows current slide
 function showslide(slideIndex) {
     let slides = document.getElementsByClassName("field__item");
     let i = 0
     for (i in slides.length-1){slides[i].style.visibility = "hidden";}
     slides[slideIndex].style.display = "visible";
 }
+// executed main function on content load
 function main() {
+    timerID = autoswitch();
     autoswitch();
-};
+}
