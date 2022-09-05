94864a87e2b3b9142a044ae819b1414b7bfa7c65
SkeL <maihoernchen.index@gmail.com>
2022-09-05 13:34:04 +0200

    duck the zucc

--- theme/js/slideshow.js
+++ theme/js/slideshow.js
@@ -1,13 +1,4 @@
 addEventListener('load',main());
-function showslide(slideIndex,slides) {
-    for (let i=0;i < slides.length;i++) {
-        slides[i].style.display = "none";
-        console.log("Slides[i]: " ,slides[i]);
-    };
-    slides[slideIndex].style.display = "inline";
-    console.log(slideIndex)
-    console.log(slides[slideIndex]);
-}
 //function slideSwitch() {
 //    if (slideIndex == slidearray.length-2) {
 //        slideIndex = 0;
@@ -17,22 +8,25 @@ function showslide(slideIndex,slides) {
 //    showslide(slideIndex,slides);
 //}
 // imideatly switches slides
+let slideIndex = 0;
 
-function toggle(slideIndex) {
-    if (slideIndex == undefined) {
-        console.log("defined slideindex");
-        let slideIndex = 0;
-    };
+function toggle() {
     let slides = document.querySelectorAll("#titelbild .field__item");
-    console.log(slides)
+    console.log(slides);
+    console.log(slideIndex);
     if (slideIndex == slides.length-1) {
         slideIndex = 0;
     } else {
         slideIndex += 1;
     }
-    showslide(slideIndex,slides)
+    for (let i=0;i < slides.length;i++) {
+        slides[i].style.display = "none";
+        console.log("Slides[i]: " ,slides[i]);
+    };
+    slides[slideIndex].style.display = "inline";
+    console.log(slideIndex)
+    console.log(slides[slideIndex]);
 }
 function main() {
     let autoswitch = setInterval(toggle, 5000);
-
 }
