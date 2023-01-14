c15192b7405a7245b75e28ba3378bd1b0d5dbb12
TenkouNoKitsune <66792136+TenkouNoKitsune@users.noreply.github.com>
2023-01-14 19:13:55 +0100

    fixed slideshow

--- theme/js/slideshow.js
+++ theme/js/slideshow.js
@@ -1,71 +1,67 @@
-addEventListener('load',ontheload());
-document.getElementById('fwbutton').onclick = fwimtog();
-document.getElementById('bwbutton').onclick = bwimtog();
+document.addEventListener('load', ontheload());
+
+var autoswitch = null;
 
 // imideatly switches slidess
-let slideIndex = 1;
+var slideIndex = 1;
 function toggle(fw) {
     let slides = document.querySelectorAll("#titelbild .field__item");
-    if (slides.length<=1){
+    if (slides.length <= 1) {
         return true
     }
     console.log(slides);
     console.log(slideIndex);
-    if(fw){
-        if (slideIndex == slides.length-1) {
+    if (fw) {
+        if (slideIndex == slides.length - 1) {
             slideIndex = 0;
         } else {
             slideIndex += 1;
         }
     } else {
         if (slideIndex == 0) {
-            slideIndex = slides.length-1;
+            slideIndex = slides.length - 1;
         } else {
             slideIndex -= 1;
         }
     }
-    for (let i=0;i < slides.length;i++) {
+    for (let i = 0; i < slides.length; i++) {
         if (i != slideIndex) {
-            for (let z=100;z > 0;z-=1) {
-                slides[i].style.opacity = z;
-                console.log("OpacityOfInactive: ",slides[i].style.opacity);            
-            }
-            console.log("Slides[i]: " ,slides[i]);
+            slides[i].classList.add("slideshow-hidden");
+            console.log("Slides[i]: ", slides[i]);
         } else {
-            for (let m=0;m < 100;m-=1) {
-                slides[i].style.opacity = m;
-                console.log("OpacityOfActive: ",slides[i].style.opacity);
-            }
+            slides[i].classList.remove("slideshow-hidden");
         }
     };
     console.log(slideIndex)
     console.log(slides[slideIndex]);
 }
-function togglefw(){
+function togglefw() {
     toggle(true);
 }
-function togglebw(){
+function togglebw() {
     toggle(false);
 }
 function main() {
-    let autoswitch = setInterval(togglefw, 5000);
+    autoswitch = setInterval(togglefw, 5000);
 }
-function fwimtog(){
+function fwimtog() {
     clearInterval(autoswitch);
     togglefw();
     main();
 }
-function bwimtog(){
+function bwimtog() {
     clearInterval(autoswitch);
     togglebw();
     main();
 }
-function ontheload(){
-    const fwbutton = document.createElement('fwbutton');
+function ontheload() {
+    const fwbutton = document.createElement('button');
     fwbutton.innerText = '>';
-    const bwbutton = document.createElement('bwbutton');
+    const bwbutton = document.createElement('button');
     bwbutton.innerText = '<';
+    fwbutton.onclick = fwimtog();
+    bwbutton.onclick = bwimtog();
     document.getElementById("titelbild").appendChild(fwbutton);
-    document.getElementById("titelbild").appendChild(bwbutton);   
+    document.getElementById("titelbild").appendChild(bwbutton);
     setTimeout(main, 2000);
 }
