217aae1e0d5758e6693e04a6304feaec698d7814
TenkouNoKitsune <66792136+TenkouNoKitsune@users.noreply.github.com>
2023-04-13 20:23:41 +0200

    prevented mousepointer from getting stuck on map
    also moved the adding of map tiles to the top so that the map is loaded first and then the pins

--- theme/js/standortkarte.js
+++ theme/js/standortkarte.js
@@ -1,19 +1,46 @@
 window.onload = () => {
 
+    const mapContainer = document.getElementById("map");
+    // [top left (x), top left (y), bottom right (x), bottom right (y)]
+    const mapDim = [mapContainer.offsetLeft, mapContainer.offsetTop, mapContainer.offsetLeft + mapContainer.offsetWidth, mapContainer.offsetTop + mapContainer.offsetHeight];
+
+    // map is 'deactivated' by default to prevent the mousepointer getting stuck on the map in an attempt to scroll the page
+    function activateMap() {
+        mapContainer.classList.remove("deactivated");
+        document.body.removeEventListener("mousedown", checkActivateMap); // event listener not needed anymore
+    }
+
+    function checkActivateMap(e) {
+        scrollY, scrollX = document.body.scrollTop, document.body.scrollLeft;
+        // checks if user clicked on map (pointer events are disabled by css, so the position is checked)
+        if (e.clientX >= mapDim[0] - scrollX && e.clientX <= mapDim[2] - scrollX && e.clientY >= mapDim[1] - scrollY && e.clientY <= mapDim[3] - scrollY) {
+            activateMap();
+        }
+    }
+
+    document.body.addEventListener("mousedown", checkActivateMap);
+
     const map = L.map('map').setView([52.5670, 13.4950], 14);
+
+    // https://leafletjs.com/reference.html
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
     const gebaeude_divs = document.querySelectorAll(".site");
     const gebaeude_sort = ["HG", "FG1", "FG2", "FG3", "TH4", "SpH"];
-    var FG1 = [52.56992, 13.49212, "Fontane Gebäude 1","Doberaner Str. 58, 13051 Berlin"];
-    var FG2 = [52.56921, 13.49181, "Fontane Gebäude 2","Doberaner Str. 55, 13051 Berlin"];
-    var FG3 = [52.56872, 13.49251, "Fontane Gebäude 3","Doberaner Str. 53, 13051 Berlin"];
-    var HG = [52.57212, 13.48466, "Hauptgebäude","Malchower Ch 2, 13051 Berlin"];
-    var SpH = [52.55679, 13.48977, "Sportplatz Hansastr.","Hansastraße 190, 13088 Berlin"];
-    var TH4 = [52.57204, 13.50318, "Turnhalle FG4","Ribnitzer Str. 1, 13051 Berlin"];
+    var FG1 = [52.56992, 13.49212, "Fontane Gebäude 1", "Doberaner Str. 58, 13051 Berlin"];
+    var FG2 = [52.56921, 13.49181, "Fontane Gebäude 2", "Doberaner Str. 55, 13051 Berlin"];
+    var FG3 = [52.56872, 13.49251, "Fontane Gebäude 3", "Doberaner Str. 53, 13051 Berlin"];
+    var HG = [52.57212, 13.48466, "Hauptgebäude", "Malchower Ch 2, 13051 Berlin"];
+    var SpH = [52.55679, 13.48977, "Sportplatz Hansastr.", "Hansastraße 190, 13088 Berlin"];
+    var TH4 = [52.57204, 13.50318, "Turnhalle FG4", "Ribnitzer Str. 1, 13051 Berlin"];
 
     var markerFG1 = L.marker([FG1[0], FG1[1]]).addTo(map);
     var markerFG2 = L.marker([FG2[0], FG2[1]]).addTo(map);
     var markerFG3 = L.marker([FG3[0], FG3[1]]).addTo(map);
-    var markerHG = L.marker([HG[0], HG[1]],{title: HG[2]}).addTo(map);
+    var markerHG = L.marker([HG[0], HG[1]], { title: HG[2] }).addTo(map);
     var markerSpH = L.marker([SpH[0], SpH[1]]).addTo(map);
     var markerTH4 = L.marker([TH4[0], TH4[1]]).addTo(map);
 
@@ -45,6 +72,7 @@ window.onload = () => {
     function markSite(marker, site, scrollToMap = true) {
         if (scrollToMap) {
             document.getElementById("map").scrollIntoView();
+            activateMap();
         }
         let popupText = "<b>" + site[2] +
             "</b><br><p>Route: <a href=https://www.openstreetmap.org/directions?from=&to=" +
@@ -112,11 +140,5 @@ window.onload = () => {
     markerHG.on("mousedown", markSite(markerHG, HG, false));
     markerSpH.on("mousedown", markSite(markerSpH, SpH, false));
     markerTH4.on("mousedown", markSite(markerTH4, TH4, false));
-
-    // https://leafletjs.com/reference.html
-    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
 
 };
